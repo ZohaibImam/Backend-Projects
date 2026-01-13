fa04fce6cfd4237137caef2072cb9f275fbb8393
Zohaib Imam <zaid.imam123@gmail.com>
2026-01-13 23:20:34 +0530

    small fix to export models

--- Trainza/db.js
+++ Trainza/db.js
@@ -21,3 +21,10 @@ const userModel = mongoose.model("user", userSchema);
 const adminModel = mongoose.model("admin", adminSchema);
 const courseModel = mongoose.model("course", courseSchema);
 const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel : userModel,
+    adminModel : adminModel,
+    courseModel : courseModel,
+    purchaseModel : purchaseModel
+};
