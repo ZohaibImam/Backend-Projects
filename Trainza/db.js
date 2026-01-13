ea6117f63c4204d10c30ff04fa41a7b7219bcdaa
Zohaib Imam <zaid.imam123@gmail.com>
2026-01-13 23:17:43 +0530

    Created db.js file for database and written the skeleton of the database schema, created the models for it

---
+++ Trainza/db.js
@@ -0,0 +1,23 @@
+const mongoose  = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+
+})
+
+const adminSchema = new Schema ({
+    
+})
+
+const courseSchema = new Schema ({
+    
+})
+
+const purchaseSchema = new Schema ({
+    
+})
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
