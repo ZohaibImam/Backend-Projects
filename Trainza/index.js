9eb00bb5f359dcdee49ae2da159530231ae226a1
Zohaib Imam <zaid.imam123@gmail.com>
2026-01-13 22:01:49 +0530

    required the admin.js file routes to use it

--- Trainza/index.js
+++ Trainza/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const {userRouter} = require("./routes/user");
+const {adminRouter} = require("./routes/admin");
 const {courseRouter} = require("./routes/course");
 
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
 const app = express();
